c23b4dab8b4400834dc590c9806d753ee99f78c0
alyssadmoore <alyssadmoore@gmail.com>
2017-04-04 15:06:35 -0500

    Add edit button: right now, only adds new post

--- LMNOPsite/lmn/views_notes.py
+++ LMNOPsite/lmn/views_notes.py
@@ -15,12 +15,12 @@ def new_note(request, show_pk):
 
     show = get_object_or_404(Show, pk=show_pk)
 
-    if request.method == 'POST' :
+    if request.method == 'POST':
 
         form = NewNoteForm(request.POST)
         if form.is_valid():
 
-            note = form.save(commit=False);
+            note = form.save(commit=False)
             if note.title and note.text:  # If note has both title and text
                 note.user = request.user
                 note.show = show
@@ -28,16 +28,15 @@ def new_note(request, show_pk):
                 note.save()
                 return redirect('lmn:note_detail', note_pk=note.pk)
 
-    else :
+    else:
         form = NewNoteForm()
 
-    return render(request, 'lmn/notes/new_note.html' , { 'form' : form , 'show':show })
-
+    return render(request, 'lmn/notes/new_note.html', {'form': form, 'show': show})
 
 
 def latest_notes(request):
     notes = Note.objects.all().order_by('posted_date').reverse()
-    return render(request, 'lmn/notes/note_list.html', {'notes':notes})
+    return render(request, 'lmn/notes/note_list.html', {'notes': notes})
 
 
 def notes_for_show(request, show_pk):   # pk = show pk
@@ -46,10 +45,31 @@ def notes_for_show(request, show_pk):   # pk = show pk
     notes = Note.objects.filter(show=show_pk).order_by('posted_date').reverse()
     show = Show.objects.get(pk=show_pk)  # Contains artist, venue
 
-    return render(request, 'lmn/notes/note_list.html', {'show': show, 'notes':notes } )
-
+    return render(request, 'lmn/notes/note_list.html', {'show': show, 'notes': notes})
 
 
 def note_detail(request, note_pk):
     note = get_object_or_404(Note, pk=note_pk)
-    return render(request, 'lmn/notes/note_detail.html' , {'note' : note })
+    return render(request, 'lmn/notes/note_detail.html', {'note': note})
+
+
+@login_required
+# TODO user validation
+def note_edit(request, note_pk):
+
+    note = get_object_or_404(Note, pk=note_pk)
+    if request.method == 'POST':
+
+        form = NewNoteForm(request.POST, instance=note)
+        if form.is_valid():
+
+            note = form.save(commit=False)
+            if note.title and note.text:
+
+                note.posted_date = timezone.now()
+                note.save()
+                return redirect('lmn:note_detail', note_pk=note.pk)
+    else:
+        form = NewNoteForm(instance=note)
+
+    return render(request, 'lmn/notes/new_note.html', {'form': form, 'show': note.show})
