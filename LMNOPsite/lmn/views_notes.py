7c6e60f8e90a9103219b60652dc1e19c9b6c95c4
alyssadmoore <alyssadmoore@gmail.com>
2017-04-08 15:27:47 -0500

    Debugging note edit

--- LMNOPsite/lmn/views_notes.py
+++ LMNOPsite/lmn/views_notes.py
@@ -59,6 +59,7 @@ def note_edit(request, note_pk):
 
     note = get_object_or_404(Note, pk=note_pk)
     show = note.show
+
     if request.method == 'POST':
 
         form = NoteForm(request.POST, instance=note)
@@ -72,4 +73,4 @@ def note_edit(request, note_pk):
     else:
         form = NoteForm(instance=note)
 
-    return render(request, 'lmn/notes/note_edit.html', {'form': form, 'note_pk': note.pk, 'show': show.pk})
+    return render(request, 'lmn/notes/new_note.html', {'form': form, 'show': show.pk})
