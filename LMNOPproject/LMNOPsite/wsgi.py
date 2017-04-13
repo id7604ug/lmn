6a98335c1709bf92531bf85a8e03bfccac70c22c
Anthony <@home>
2017-04-13 14:51:30 -0500

    Updated wsgi for Heroku

--- LMNOPproject/LMNOPsite/wsgi.py
+++ LMNOPproject/LMNOPsite/wsgi.py
@@ -1,8 +1,6 @@
 """
 WSGI config for LMNOPsite project.
-
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
@@ -10,10 +8,14 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LMNOPsite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LMNOPproject.LMNOPsite.settings")
 
 application = get_wsgi_application()
+
+
+# Static files for Heroku
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 application = DjangoWhiteNoise(application)
