b7d0f6d5975339226b8ddba3d50988dc2e8946f4
Anthony <@home>
2017-04-13 15:00:02 -0500

    Edited wsgi

--- LMNOPproject/LMNOPsite/wsgi.py
+++ LMNOPproject/LMNOPsite/wsgi.py
@@ -9,7 +9,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lmn.LMNOPsite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LMNOPproject.LMNOPsite.settings")
 
 application = get_wsgi_application()
 
